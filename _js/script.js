81220d522c7bf735bc7a656447f348e0a0686970
sabrina <bina.oliver4@gmail.com>
2021-02-03 17:00:27 -0300

    font-family

--- _js/script.js
+++ _js/script.js
@@ -3,20 +3,20 @@ setInterval(function(){
     let topo = document.getElementById("scroll").getBoundingClientRect().top
     console.log(topo)
     if(topo <= (window.innerHeight - 100)){
-        $(".cont-nav").slideDown(1000);
+        $(".cont-nav").slideDown(500);
         if(topo <= (window.innerHeight - 300)){
             $(".div-anima").addClass("position")
         }
     }else{
-        $(".cont-nav").slideUp(0);
+        $(".cont-nav").slideUp(350);
     }
 },1000)
 
 /* menu media querie */
 $("#bars").click(function(){
-    $("#ul-m").slideToggle(1500)
+    $("#ul-m").slideToggle(700)
     $(".li-menu a").click(function(){
-        $("#ul-m").slideUp(500)
+        $("#ul-m").slideUp(350)
     })
 
 })
