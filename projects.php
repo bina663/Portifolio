7ecc8ad3082f134c58383e3d1adf07bb98333048
Sabrina <bina.oliver4@gmail.com>
2023-02-20 19:32:10 -0300

    update

--- projects.php
+++ projects.php
@@ -1,18 +1,18 @@
 <?php
     $projects = array(
         
-        ['name' => 'Loja de Games','description' => "Um sistema de compra de jogos, onde após<br>realizar o pedido um email sera enviado.", 'image' => "lojagames.jpg", 'link' => "http://portfoliosabrinaoliver-shop.preview-domain.com/lojagames/"],
+        ['name' => 'Loja de Games','description' => "Um sistema de compra de jogos, onde após<br>realizar o pedido um email sera enviado.", 'image' => "lojagames.jpg", 'link' => "https://portfoliosabrinaoliver.shop/lojagames/"],
         
         ['name' => 'AHS: Asylum','description' => "Projeto feito pra concretizar meus conhecimentos sobre API,<br>pra isso crei uma API com <strong>Node.js</strong>, e esse projeto consome essa API criada.", 'image' => "asylum.png", 'link' => 'https://fierce-necklace-goat.cyclic.app/'],
 
-        ['name' => 'Api Rick and Morty','description' => "Esse projeto consome uma API da serie em questão, e é possivel filtrar os personagens<br>por alguma categoria especifica.", 'image' => "api_rick_morty.jpg", 'link' => 'http://portfoliosabrinaoliver-shop.preview-domain.com/apiRickyMorty/'],
+        ['name' => 'Api Rick and Morty','description' => "Esse projeto consome uma API da serie em questão, e é possivel filtrar os personagens<br>por alguma categoria especifica.", 'image' => "api_rick_morty.jpg", 'link' => 'https://portfoliosabrinaoliver.shop/apirickymorty/'],
 
         
-        ['name' => 'Formulario PHP','description' => "Projeto com intuito de praticar meus conhecimentos em <strong>PHP</strong>.", 'image' => "formulario.jpg", 'link' => "http://portfoliosabrinaoliver-shop.preview-domain.com/formulario/"],
+        ['name' => 'Formulario PHP','description' => "Projeto com intuito de praticar meus conhecimentos em <strong>PHP</strong>.", 'image' => "formulario.jpg", 'link' => "https://portfoliosabrinaoliver.shop/loginphp/"],
 
         ['name' => 'Coffe Starburcks','description' => "Projeto de Landing Page com fundo dinâmico.", 'image' => "coffe.jpg", 'link' => "https://eager-torvalds-e78d71.netlify.app/"],
 
-        ['name' => 'Seguro de Celular','description' => "Projeto de Landing Page<br>de Seguro de Celular.", 'image' => "seguro_celular.jpg", 'link' => "https://mystifying-rosalind-e142e5.netlify.app/"],
+        ['name' => 'Clone Spotify','description' => "Projeto de Landing Page<br>Clone Spotify.", 'image' => "spotify.jpg", 'link' => "https://whimsical-crepe-f823c7.netlify.app/"],
         
         ['name' => 'The Beatles','description' => "Fiz esse projeto no começo dos meus estudos<br>e refiz pouco tempo atrás, e um projeto simples<br>onde pratiquei HTML e CSS.", 'image' => "beatles.jpg", 'link' => "https://dreamy-jennings-da804b.netlify.app/index.html"],
 
